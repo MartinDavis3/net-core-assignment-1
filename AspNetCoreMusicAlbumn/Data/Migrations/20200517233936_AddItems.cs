5b6462b4074739eefe9ee047322e89dcbec8fbee
Martin Davis <martin_davis3@yahoo.ca>
2020-05-17 17:46:44 -0600

    applied database migration

---
+++ AspNetCoreMusicAlbumn/Data/Migrations/20200517233936_AddItems.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetCoreMusicAlbumn.Data.Migrations
+{
+    public partial class AddItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Artist = table.Column<string>(nullable: true),
+                    Year = table.Column<int>(nullable: true),
+                    NumberOfTracks = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
